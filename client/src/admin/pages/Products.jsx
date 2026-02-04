70c698693d564adb73af8e56761613674c840b37
BlacqMan <donkoremmanuelopoku@email.com>
2026-02-04 03:09:13 +0000

    Fix axios import paths for production build

--- client/src/admin/pages/Products.jsx
+++ client/src/admin/pages/Products.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import api from "../../api/axios";
+import axios from "../../api/axios"
 
 const Products = () => {
   const [products, setProducts] = useState([]);
